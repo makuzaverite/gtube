7a8ff04e4191619fc911fee444504cb63e807fea
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-09 22:15:15 +0200

    extracting video id from the url

--- cmd/video.go
+++ cmd/video.go
@@ -1,6 +1,6 @@
 package cmd
 
 //DownloadDefaultVideo  a  video when url is specified from youtube
-func DownloadDefaultVideo(url string) {
-	GetVideoIDFromURL(url)
-}
+// func DownloadDefaultVideo(url string) {
+// 	GetVideoIDFromURL(url)
+// }
